91a26e5e2d2eaade45d9b75ba8ce8513bada2fc5
Raúl (Station) Pérez <raulytstation@Outlook.com>
2022-11-21 17:43:20 -0400

    feat: Add shiki configuration
    Add support for:
    c
    js
    ts
    tsx
    jsx
    astro
    md
    mdx
    rs
    sh
    xml
    py
    lua
    css
    
    Change theme from default to rose-pine-moon

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,11 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://blog.stationarystation.vercel.app',
-	integrations: [mdx(), sitemap()],
+	markdown: {
+    shikiConfig: {
+      theme: "rose-pine-moon",
+      langs: ["c", "js", "ts", "jsx", "astro", "md", "mdx", "rs", "sh", "xml", "py", "lua", "css", "tsx"]
+    }
+  },
+  integrations: [mdx(), sitemap()],
 });
