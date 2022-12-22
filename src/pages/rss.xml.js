42157f1f353808cad06e0e0b6531d1a26e60ec20
StationaryStation <raulytstation@outlook.com>
2022-12-22 18:25:48 -0400

    fix: Rss feed not building

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,9 @@
 import rss from '@astrojs/rss';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
+import sanitizeHtml from 'sanitize-html';
+
+const postImportResult = import.meta.glob('./blog/**/*.md', { eager: true }).filter((post) => !post.frontmatter.draft);
+const posts = Object.values(postImportResult);
 
 export const get = () =>
 	rss({
@@ -7,5 +11,10 @@ export const get = () =>
 		description: SITE_DESCRIPTION,
 		site: import.meta.env.SITE,
 		// Don't include drafts please thank u
-		items: import.meta.glob('./blog/**/*.{md,mdx}').filter((post) => !post.frontmatter.draft),
+		items: posts.map((post) => ({
+			link: post.url,
+			title: post.frontmatter.title,
+			pubDate: post.frontmatter.pubDate,
+			content: sanitizeHtml(post.compiledContent()),
+		})),
 	});
