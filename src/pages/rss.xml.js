f25e5d77fb97e0282009c14b9c5236024c7b7de0
StationaryStation <raulytstation@outlook.com>
2022-12-22 18:34:04 -0400

    fix: turns out it just ignores drafts by default lmao

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,9 +1,5 @@
 import rss from '@astrojs/rss';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
-import sanitizeHtml from 'sanitize-html';
-
-const postImportResult = import.meta.glob('./blog/*.md', { eager: true }).filter((post) => !post.frontmatter.draft);
-const posts = Object.values(postImportResult);
 
 export const get = () =>
 	rss({
@@ -11,10 +7,5 @@ export const get = () =>
 		description: SITE_DESCRIPTION,
 		site: import.meta.env.SITE,
 		// Don't include drafts please thank u
-		items: posts.map((post) => ({
-			link: post.url,
-			title: post.frontmatter.title,
-			pubDate: post.frontmatter.pubDate,
-			content: sanitizeHtml(post.compiledContent()),
-		})),
+		items: import.meta.glob('./blog/*.{md,mdx}'),
 	});
